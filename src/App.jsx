220e3dadbcb12f70c660111cf9560780c50d3da7
Domatiara <michellnandara2000@gmail.com>
2025-07-08 10:21:40 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export default function App() {
+  
+  return (
+    <>
+      <h1 className='text-amber-300 font-extrabold text-4xl'>Welcome Home</h1>
+      <p className='text-green-700 font-medium text-2xl'>Home is what you make it. There's no place like home.</p>
+    </>
+  )
+}
+
